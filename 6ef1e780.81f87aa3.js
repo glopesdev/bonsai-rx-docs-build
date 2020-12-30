1fa1d1280f0e06a84d7c2bbc2156d67f3b3ca700
glopesdev <glopesdev@users.noreply.github.com>
2020-12-30 16:08:48 +0000

    Add static build

---
+++ 6ef1e780.81f87aa3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{89:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Crowdsourced Documentation!","permalink":"/blog/first_post"}]}')}}]);
