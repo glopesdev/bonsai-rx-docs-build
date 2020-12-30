76c2bbf1d2b96133388057f9eeda79b2c3f82c97
glopesdev <glopesdev@users.noreply.github.com>
2020-12-30 16:16:47 +0000

    Update build

---
+++ 6ef1e780.f176cbcc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{89:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Crowdsourced Documentation!","permalink":"/bonsai-rx-docs-build/blog/first_post"}]}')}}]);
